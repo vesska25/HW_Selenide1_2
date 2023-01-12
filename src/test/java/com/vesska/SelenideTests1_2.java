1a8fc8f8da933bdbf2e6fd09d34250dc362ec760
Sergei Savich <s.savich@ipoteka.center>
2023-01-12 19:47:16 +0300

    renamed test

--- src/test/java/com/vesska/SelenideTests1_2.java
+++ src/test/java/com/vesska/SelenideTests1_2.java
@@ -8,7 +8,7 @@ import static com.codeborne.selenide.Selectors.withText;
 import static com.codeborne.selenide.Selenide.*;
 import static io.qameta.allure.Allure.step;
 
-public class GithubSolutionsTest {
+public class SelenideTests1_2 {
 
 
     @Test
